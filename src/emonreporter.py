92b7b405809932d9cab3f97f92cbcbc3bef8f792
ianhorsley <ianspam@horsley.me.uk>
2018-06-22 16:39:01 +0100

    updated for newer library version

--- src/emonreporter.py
+++ src/emonreporter.py
@@ -64,11 +64,11 @@ class EmonReporter(object):
         self._log.info("EmonReporter %s" % self.__version__)
         self._log.info("Opening reporter...")
         
-        # # Initialize Reporters and Interfacers
-        # self._reporters = {}
-        # self._interfacers = {}
-        # self._queue = {}
-        # self._update_settings(settings)
+        # Initialize Interfacers
+        self._interfacers = {}
+
+        # Update settings
+        self._update_settings(settings)
         
     def run(self):
         """Launch the reporter.
@@ -131,126 +131,74 @@ class EmonReporter(object):
         # reporter should exit at the end of current iteration.
         self._exit = True
 
-    # def _update_settings(self, settings):
-        # """Check settings and update if needed."""
+    def _update_settings(self, settings):
+        """Check settings and update if needed."""
 
-        # # EmonHub Logging level
-        # if 'loglevel' in settings['hub']:
-            # self._set_logging_level(settings['hub']['loglevel'])
-        # else:
-            # self._set_logging_level()
+        # EmonHub Logging level
+        if 'loglevel' in settings['hub']:
+            self._set_logging_level(settings['hub']['loglevel'])
+        else:
+            self._set_logging_level()
 
-        # # Create a place to hold buffer contents whilst a deletion & rebuild occurs
-        # self.temp_buffer = {}
-        
-        # # Reporters
-        # for name in self._reporters.keys():
-            # # Delete reporters if not listed or have no 'Type' in the settings without further checks
-            # # (This also provides an ability to delete & rebuild by commenting 'Type' in conf)
-            # if not name in settings['reporters'] or not 'Type' in settings['reporters'][name]:
-                # pass
-            # else:
-                # try:
-                    # # test for 'init_settings' and 'runtime_setting' sections
-                    # settings['reporters'][name]['init_settings']
-                    # settings['reporters'][name]['runtimesettings']
-                # except Exception as e:
-                    # # If reporter's settings are incomplete, continue without updating
-                    # self._log.error("Unable to update '" + name + "' configuration: " + str(e))
-                    # continue
-                # else:
-                    # # check init_settings against the file copy, if they are the same move on to the next
-                    # if self._reporters[name].init_settings == settings['reporters'][name]['init_settings']:
-                        # continue
-                    # else:
-                        # if self._reporters[name].buffer._data_buffer:
-                            # self.temp_buffer[name]= self._reporters[name].buffer._data_buffer
-            # # Delete reporters if setting changed or name is unlisted or Type is missing
-            # self._log.info("Deleting reporter '%s'", name)
-            # self._reporters[name].stop = True
-            # del(self._reporters[name])
-        # for name, R in settings['reporters'].iteritems():
-            # # If reporter does not exist, create it
-            # if name not in self._reporters:
-                # try:
-                    # if not 'Type' in R:
-                        # continue
-                    # self._log.info("Creating " + R['Type'] + " '%s' ", name)
-                    # # Create the queue for this reporter
-                    # self._queue[name] = Queue.Queue(0)
-                    # # This gets the class from the 'Type' string
-                    # reporter = getattr(ehr, R['Type'])(name, self._queue[name], **R['init_settings'])
-                    # reporter.set(**R['runtimesettings'])
-                    # reporter.init_settings = R['init_settings']
-                    # # If a memory buffer back-up exists copy it over and remove the back-up
-                    # if name in self.temp_buffer:
-                        # reporter.buffer._data_buffer = self.temp_buffer[name]
-                        # del self.temp_buffer[name]
-                # except ehr.EmonHubReporterInitError as e:
-                    # # If reporter can't be created, log error and skip to next
-                    # self._log.error("Failed to create '" + name + "' reporter: " + str(e))
-                    # continue
-                # except Exception as e:
-                    # # If reporter can't be created, log error and skip to next
-                    # self._log.error("Unable to create '" + name + "' reporter: " + str(e))
-                    # continue
-                # else:
-                    # self._reporters[name] = reporter
-            # else:
-                # # Otherwise just update the runtime settings if possible
-                # if 'runtimesettings' in R:
-                    # self._reporters[name].set(**R['runtimesettings'])
 
-        # # Interfacers
-        # for name in self._interfacers.keys():
-            # # Delete interfacers if not listed or have no 'Type' in the settings without further checks
-            # # (This also provides an ability to delete & rebuild by commenting 'Type' in conf)
-            # if not name in settings['interfacers'] or not 'Type' in settings['interfacers'][name]:
-                # pass
-            # else:
-                # try:
-                    # # test for 'init_settings' and 'runtime_setting' sections
-                    # settings['interfacers'][name]['init_settings']
-                    # settings['interfacers'][name]['runtimesettings']
-                # except Exception as e:
-                    # # If interfacer's settings are incomplete, continue without updating
-                    # self._log.error("Unable to update '" + name + "' configuration: " + str(e))
-                    # continue
-                # else:
-                    # # check init_settings against the file copy, if they are the same move on to the next
-                    # if self._interfacers[name].init_settings == settings['interfacers'][name]['init_settings']:
-                        # continue
-            # self._interfacers[name].close()
-            # self._log.info("Deleting interfacer '%s' ", name)
-            # del(self._interfacers[name])
-        # for name, I in settings['interfacers'].iteritems():
-            # # If interfacer does not exist, create it
-            # if name not in self._interfacers:
-                # try:
-                    # if not 'Type' in I:
-                        # continue
-                    # self._log.info("Creating " + I['Type'] + " '%s' ", name)
-                    # # This gets the class from the 'Type' string
-                    # interfacer = getattr(ehi, I['Type'])(name, **I['init_settings'])
-                    # interfacer.set(**I['runtimesettings'])
-                    # interfacer.init_settings = I['init_settings']
-                # except ehi.EmonHubInterfacerInitError as e:
-                    # # If interfacer can't be created, log error and skip to next
-                    # self._log.error("Failed to create '" + name + "' interfacer: " + str(e))
-                    # continue
-                # except Exception as e:
-                    # # If interfacer can't be created, log error and skip to next
-                    # self._log.error("Unable to create '" + name + "' interfacer: " + str(e))
-                    # continue
-                # else:
-                    # self._interfacers[name] = interfacer
-            # else:
-                # # Otherwise just update the runtime settings if possible
-                # if 'runtimesettings' in I:
-                    # self._interfacers[name].set(**I['runtimesettings'])
+        # Create a place to hold buffer contents whilst a deletion & rebuild occurs
+        self.temp_buffer = {}
 
-        # if 'nodes' in settings:
-            # ehc.nodelist = settings['nodes']
+        # Interfacers
+        for name in self._interfacers.keys():
+            # Delete interfacers if not listed or have no 'Type' in the settings without further checks
+            # (This also provides an ability to delete & rebuild by commenting 'Type' in conf)
+            if not name in settings['interfacers'] or not 'Type' in settings['interfacers'][name]:
+                pass
+            else:
+                try:
+                    # test for 'init_settings' and 'runtime_setting' sections
+                    settings['interfacers'][name]['init_settings']
+                    settings['interfacers'][name]['runtimesettings']
+                except Exception as e:
+                    # If interfacer's settings are incomplete, continue without updating
+                    self._log.error("Unable to update '" + name + "' configuration: " + str(e))
+                    continue
+                else:
+                    # check init_settings against the file copy, if they are the same move on to the next
+                    if self._interfacers[name].init_settings == settings['interfacers'][name]['init_settings']:
+                        continue
+            # Delete interfacers if setting changed or name is unlisted or Type is missing
+            self._log.info("Deleting interfacer '%s' ", name)
+            self._interfacers[name].stop = True
+            del(self._interfacers[name])
+
+        for name, I in settings['interfacers'].iteritems():
+            # If interfacer does not exist, create it
+            if name not in self._interfacers:
+                try:
+                    if not 'Type' in I:
+                        continue
+                    self._log.info("Creating " + I['Type'] + " '%s' ", name)
+                    if I['Type'] in ('EmonModbusTcpInterfacer','EmonFroniusModbusTcpInterfacer') and not pymodbus_found :
+                        self._log.error("Python module pymodbus not installed. unable to load modbus interfacer")
+                    # This gets the class from the 'Type' string
+                    interfacer = getattr(ehi, I['Type'])(name,**I['init_settings'])
+                    interfacer.set(**I['runtimesettings'])
+                    interfacer.init_settings = I['init_settings']
+                    interfacer.start()
+                except ehi.EmonHubInterfacerInitError as e:
+                    # If interfacer can't be created, log error and skip to next
+                    self._log.error("Failed to create '" + name + "' interfacer: " + str(e))
+                    continue
+                except Exception as e:
+                    # If interfacer can't be created, log error and skip to next
+                    self._log.error("Unable to create '" + name + "' interfacer: " + str(e))
+                    continue
+                else:
+                    self._interfacers[name] = interfacer
+            else:
+                # Otherwise just update the runtime settings if possible
+                if 'runtimesettings' in I:
+                    self._interfacers[name].set(**I['runtimesettings'])
+
+        if 'nodes' in settings:
+            ehc.nodelist = settings['nodes']
 
     def _set_logging_level(self, level='WARNING', log=True):
         """Set logging level.
@@ -287,7 +235,7 @@ if __name__ == "__main__":
     
     # Configuration file
     parser.add_argument("--config-file", action="store",
-                        help='Configuration file', default=sys.path[0]+'/../conf/emonhub.conf')
+                        help='Configuration file', default=sys.path[0]+'/../conf/emonreporter.conf')
     # Log file
     parser.add_argument('--logfile', action='store', type=argparse.FileType('a'),
                         help='Log file (default: log to Standard error stream STDERR)')
---
+++ src/rept_1wire_hm.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+
+# program to interogate 1-wire bus and report all known temperatures
+# with some commentary
+
+# by ian
+# This code has no warranty, including any implied fitness for purpose
+# or implied merchantability.  It's free, and worth every penny.
+# 2018/11/02
+# updated to add more error catching and remove trend code
+
+# /lib/systemd/system/1wireemon.service
+# /home/pi/data/emonhub.conf
+
+# standard library modules used in code
+import sys
+import time
+import argparse
+import socket
+import logging
+# statistics from scipy
+#from scipy import stats
+import emonhub_coder
+import serial
+
+# use OWFS module
+import ow
+
+# hm imports
+from heatmisercontroller import hm_constants, network, logging_setup
+from heatmisercontroller.exceptions import hmResponseError, hmControllerTimeError
+
+#setup logging
+logfolder = '/home/pi/rept_logs'
+logging_setup.initialize_logger(logfolder, logging.WARN, True)
+
+# set up parser with command summary
+parser = argparse.ArgumentParser(
+        description='Rolling 1-wire temperatures report')
+# set up arguments with associated help and defaults 
+parser.add_argument('-i', 
+        dest='sample_interval', 
+        help='interval in seconds between samples', 
+        default='30')
+
+# process the arguments
+args=parser.parse_args()
+# turn the arguments into numbers
+sample_interval=float(args.sample_interval)
+
+# socket config for emon connection
+host = 'localhost'
+port = 50011
+node = '18' # for 1 wire bus
+hmnode = '27' # for hm stat reporting
+
+# tell the user what is happening
+logging.info("1 wire bus reporting and hmstat reporting")
+logging.info("  sample interval: "+str(sample_interval) + " seconds")
+
+### 1 wire setup
+# connect to localhost port 4304 where owserver should be running
+try:
+	ow.init('4304');
+except:
+	logging.warn('Could not connect to ow')
+
+# now determine what sensors we will consider
+logging.debug("locate sensors and take initial readings:")
+# every sensor on the bus added to the list
+rawlist = ow.Sensor('/').sensorList()
+
+# create lists for teh data we will accumulate
+sensorlist=[]
+
+# get time in seconds now
+tt=int(time.time())
+
+n=0 # initialise number of temperature sensors found so far
+logging.info("initialising one wire array")
+for s in rawlist:
+    logging.info("  consider " + str(s) + ": ")
+    if 'temperature' in s.entryList():
+        # get teh temperature from that sensor
+        T = s.temperature
+        # increment count of sensors
+        n = n + 1  
+        # tell the user teh good news
+        logging.info("T: " + T)
+        # record that as a sensor to interrogate
+        sensorlist.append(s)
+        # set found sensors to use un-cached results
+        # so will run new conversion every read cycle
+        s.useCache( False )
+        # record the time of reading and value read
+    else:
+        # give the user the bad news
+        logging.warn("no temperature value")
+    print
+
+logging.info("bus search done - " + str(n) + " temperature sensors found")
+
+###setup hm network and controllers
+logging.info("initialising hm network")
+localconfigfile = '/home/pi/emonreporter/src/hmcontroller.conf'
+hmn1 = network.HeatmiserNetwork(localconfigfile)
+
+# CYCLE THROUGH ALL CONTROLLERS
+for current_controller in hmn1.controllers:
+  logging.info("Getting all data control %2d in %s *****************************" % (current_controller._address, current_controller._long_name))
+
+  try:
+    current_controller.readAll()
+  except (hmResponseError, hmControllerTimeError) as e:
+    print "C%d in %s Failed to Read due to %s" % (current_controller._address,  current_controller._name.ljust(4), str(e))
+  else:
+    disptext = "C%d Air Temp is %.1f from type %.f and Target set to %d  Boiler Demand %d" % (current_controller.address, current_controller.readAirTemp(), current_controller.readAirSensorType(), current_controller.setroomtemp, current_controller.heatingdemand)
+    if current_controller.isHotWater():
+      print "%s Hot Water Demand %d" % (disptext, current_controller.hotwaterdemand)
+    else:
+      print disptext
+    current_controller.display_heating_schedule()
+    current_controller.display_water_schedule()
+print
+
+# now loop forever reading the identified sensors
+while 1:
+    # determine how long to wait until next interval
+    # note this will skip some if specified interval is too short
+    #  - it finds the next after now, not next after last 
+    sleeptime = sample_interval - (time.time() % sample_interval)
+    time.sleep(sleeptime) 
+
+    # get time now and record it
+    tt = int(time.time())  # we only record to integer seconds
+    
+    logging.info("Logging cyle at " + str(tt))
+    
+    try:
+        fo = open(logfolder + "/testlog"+str(int(tt/86400))+".txt","ab")
+    except IOError as e:
+        logging.warn('failed to create log file : I/O error({0}): {1}'.format(e.errno, e.strerror))
+    except: #handle other exceptions such as attribute errors
+        logging.warn("Unexpected error:", sys.exc_info()[0])
+    #reset output string
+    outputstr = node
+
+    # work through list of sensors
+    for s in sensorlist:
+        n = sensorlist.index(s)  # the array register of this sensor
+        try:  # just in case it has been unplugged
+            T=float(s.temperature)
+        except ow.exUnknownSensor:  # it has been unplugged
+            logging.info('  sensor ' + str(s) + ' gone away - just ignore')
+            logging.info('sensor ' + str(s) + ' left')
+            continue  # so we'll jump to the next in the list
+
+        # print sensor name and current value
+        logging.info( '  {!s}: {:-6.2f}'.format(s.id,T))
+        stringout = '{}:{!s}:{:+06.2f}'.format(tt,s.id,T)
+
+        fo.write(stringout)
+        fo.write("\n")
+
+        outputstr += ' {:-3.0f}'.format(T*10) 
+
+    fo.close()
+
+    ### do same for hm controllers
+
+    #force read all fields at the same time to all optimisation
+    allread = hmn1.All.readFields(['sensorsavaliable','airtemp','remoteairtemp','heatingdemand','hotwaterdemand'], 0)
+    #get demands and temps replacing nones
+    demands = [99 if row is None or row[3] is None else row[3] for row in allread]
+    hotwater = 2 if allread is None or allread[0] is None else allread[0][4]
+    temps = [-10 if temp is None else temp for temp in hmn1.All.readAirTemp()]
+    
+    logging.info('Temps ' + ' '.join(map(str,temps)))
+    logging.info('Demands ' + ' '.join(map(str,demands + [hotwater])))
+    #enocde using emonhubs own module
+    encodedtemps = [' '.join(map(str,emonhub_coder.encode("h",temp * 10 ))) for temp in temps ]
+    #zip temp and demands and join string
+    outputstr2 = ' '.join([hmnode, str(hotwater)] + ['%s %d'%pair for pair in zip(encodedtemps, demands)])
+    
+    #print outputstr2
+    try:
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.connect((host,port))
+        logging.info('socket send %s'%outputstr)
+        soc.send(outputstr + '\r\n')
+        logging.info('socket send %s'%outputstr2)
+        soc.send(outputstr2 + '\r\n')
+        soc.close()
+    except:
+        logging.warn('could not connect to emonhub')
