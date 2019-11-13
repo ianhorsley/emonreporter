5c74ad0473d1195098ae36a51ad09a3f5a80198a
ianhorsley <ianspam@horsley.me.uk>
2019-11-13 07:56:45 +0000

    add setup for build

---
+++ setup.py
@@ -0,0 +1,30 @@
+"""Build setup for package"""
+from setuptools import setup
+
+def readme():
+    """Method to add readme file as long_description."""
+    with open('README.md') as fhandle:
+        return fhandle.read()
+
+setup(name='emonreporter',
+      version='0.41',
+      description='Python implementation to process room temperatures and apply to Heatmiser network ',
+      long_description=readme(),
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+      ],
+      url='https://github.com/ianhorsley/emonreporter',
+      author='Ian Horsley',
+      #author_email='flyingcircus@example.com',
+      license='GNU v3.0',
+      packages=['emonreporter'],
+      install_requires=[
+        'requests',
+        'logging',
+        'pyserial',
+        'configobj',
+        'heatmisercontroller'
+      ],
+      test_suite="tests",
+      include_package_data=True,
+      zip_safe=False)
